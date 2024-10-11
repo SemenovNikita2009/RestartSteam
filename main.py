b18343ec82e4bf56b84c145290e00a1ef8da2ae3
Linuxoid64 <semenovnikita9999@mail.ru>
2024-10-11 15:10:21 +0300

    commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import subprocess
+import signal
+import psutil
+import os
+
+steamPName = {"steam.exe", "steamservice.exe", "steamwebhelper.exe", "GameOverlayUI.exe"}
+
+def getProcessId(processName):
+    for process in psutil.process_iter():
+        if process.name() == processName:
+            return process.pid
+
+steamP = {}
+for process in steamPName:
+    steamP[process] = getProcessId(process)
+
+for process in steamP:
+    try:
+        os.kill(steamP[process], signal.SIGTERM)
+        print(f"\"{process}\" успешно завершен.")
+    except:
+        print(f"Не удалось завершить \"{process}\"")
+
+input("Для выхода нажмите любую кнопку...")
